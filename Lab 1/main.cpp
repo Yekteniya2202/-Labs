56c1f1f40408c7774138350a1fe1309ea0152842
Михаил Бабаев <enigma322228.karpovich@yandex.ru>
2021-09-19 22:52:53 +0300

    Добавьте файлы проекта.

---
+++ Lab 1/main.cpp
@@ -0,0 +1,51 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <omp.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+#ifdef _OPENMP
+	cout << _OPENMP << endl;
+#endif
+	cout << "Proc numbers " << omp_get_num_procs() << endl;
+	int th = 1024;
+	omp_set_num_threads(th);
+	int N = 100000000;
+	double start1 = omp_get_wtime();
+	cout << start1 << endl;
+	for (int i = 0; i < N; i++) {
+		int n = 0;
+		n += i;
+	}
+	double delta1 = omp_get_wtime() - start1;
+	cout << "Single thread time is "<< delta1 << endl;
+	double start2 = omp_get_wtime();
+	cout << start2 << endl;
+	int nt = N / th;
+#pragma omp parallel
+	{
+		for (int i = 0; i < nt; i++) {
+			int n = 0;
+			n += i;
+		}
+		//cout << "Hello world from thread №" << omp_get_thread_num() << endl;
+	}
+	double delta2 = omp_get_wtime() - start2;
+	cout << "Multi thread time is " << delta2 << endl;
+//#pragma omp 
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Lab 2/main.cpp
@@ -0,0 +1,199 @@
+﻿// Lab 2.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <omp.h>
+#include <cstdlib>
+using namespace std;
+
+void task_1(double& timedelta1, double& timedelta2, int N);
+void analyse_task_1(int iterations_num, int iterations_per_session);
+
+void task_1(double& timedelta1, double& timedelta2, int N) {
+
+	srand(time(NULL));
+	volatile double lfnum1 = (double)(rand() / RAND_MAX * 10000);
+	volatile double lfnum2 = (double)(rand() / RAND_MAX * 10000);
+	volatile double res;
+
+	double start1 = omp_get_wtime();
+	for (int i = 0; i < N; i++) {
+		res = lfnum1 * lfnum2;
+	}
+	double end1 = omp_get_wtime();
+	timedelta1 = end1 - start1;
+
+	double start2 = omp_get_wtime();
+	omp_set_dynamic(0);
+	omp_set_num_threads(2);
+	#pragma omp parallel
+	{
+	#pragma omp for schedule(static)  //firstprivate(lfnum1, lfnum2)
+		for (int i = 0; i < N; i++) {
+			res = lfnum1 * lfnum2;
+		}
+	}
+
+	double end2 = omp_get_wtime();
+
+	timedelta2 = end2 - start2;
+}
+
+void analyse_task_1(int iterations_num, int iterations_per_session) {
+	cout << "Внешних итераций: " << iterations_num << endl;
+	cout << "Внутренних итераций: " << iterations_per_session << endl;
+
+	int expected_sessions = 0, unexpected_sessions = 0;
+	for (int i = 0; i < iterations_num; i++) {
+		double delta1, delta2;
+		task_1(delta1, delta2, iterations_per_session);
+		delta1 > delta2 ? expected_sessions++ : unexpected_sessions++;
+	}
+	cout << "Ожидаемых результатов: " << expected_sessions << endl;
+	cout << "Неожидаемых результатов: " << unexpected_sessions << endl;
+}
+void task_2(double& timedelta) {
+	double start = omp_get_wtime();
+	#pragma omp parallel
+	{
+	}
+	timedelta = omp_get_wtime() - start;
+}
+
+void analyse_task_2(int iterations_count) {
+	double delta;
+	for (int i = 0; i < iterations_count; i++) {
+		task_2(delta);
+		cout << "Время создания региона: " << fixed << delta << endl;
+	}
+}
+
+void task_3() {
+	#pragma omp parallel
+	{
+		#pragma omp critical
+		cout << "Начало " << omp_get_thread_num() << endl;
+	#pragma omp single
+		{
+
+		#pragma omp critical
+			cout << "Одна нить " << omp_get_thread_num() << endl;
+		}
+
+		#pragma omp critical
+		cout << "Окончание " << omp_get_thread_num() << endl;
+	}
+	cout << "\n\n================\n\n";
+	#pragma omp parallel
+	{
+		#pragma omp critical
+		cout << "Начало " << omp_get_thread_num() << endl;
+		#pragma omp single nowait
+		{
+
+			#pragma omp critical
+			cout << "Одна нить " << omp_get_thread_num() << endl;
+		}
+		#pragma omp critical
+		cout << "Окончание " << omp_get_thread_num() << endl;
+	}
+}
+
+void task_4() {
+
+	omp_set_dynamic(0);
+	omp_set_num_threads(3);
+	auto f = []() {
+		#pragma omp master
+		{
+			#pragma omp critical
+			cout << "Главный поток " << omp_get_thread_num() << endl;
+		}
+	};
+	#pragma omp parallel
+	{
+		#pragma omp critical
+		cout << "Начало " << omp_get_thread_num() << endl;
+		f();
+		#pragma omp critical
+		cout << "Середина " << omp_get_thread_num() << endl;
+		f();
+		#pragma omp critical
+		cout << "Конец " << omp_get_thread_num() << endl;
+	}
+}
+
+void task_5() {
+	int n;
+	n = 10;
+	cout << "Последовательная область: n = " << n << endl;
+	omp_set_num_threads(2);
+	#pragma omp parallel private(n)
+	{
+		n = 5;
+		#pragma omp critical
+		cout << "Параллельная область: n = " << n << endl;
+		n = omp_get_thread_num();
+		#pragma omp critical
+		cout << "Параллельная область: n = " << n << endl;
+	}
+
+	cout << "Последовательная область: n = " << n << endl;
+}
+
+void task_6() {
+	int m[5] = { 0,0,0,0,0 };
+	for (int i = 0; i < 5; i++) {
+		cout << "m[" << i << "] = " << m[i] << endl;
+	}
+
+	omp_set_dynamic(0);
+	omp_set_num_threads(5);
+	#pragma omp parallel shared(m)
+	{
+		m[omp_get_thread_num()] = omp_get_thread_num();
+	}
+	cout << "\n\n================\n\n";
+	for (int i = 0; i < 5; i++) {
+		cout << "m[" << i << "] = " << m[i] << endl;
+	}
+}
+
+void task_7() {
+	srand(time(NULL));
+	int counter = 0;
+	omp_set_num_threads(rand() % 5 + 5);
+	#pragma omp parallel reduction(+:counter)
+	{
+		#pragma omp critical
+		cout << "Поток = " << omp_get_thread_num() << " Счетчик = " << ++counter << endl;
+	}
+	cout << "Число потоков = " << counter << endl;
+}
+
+void task_8() {
+
+	srand(time(NULL));
+	int counter = 0;
+	omp_set_num_threads(4);
+	#pragma omp parallel reduction(+:counter)
+	{
+		counter = omp_get_thread_num();
+		#pragma omp critical
+		cout << "Поток = " << omp_get_thread_num() << " Счетчик = " << counter << endl;
+	}
+	cout << "Сумма номеров потоков = " << counter << endl;
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	//analyse_task_1(500, 100000);
+	//analyse_task_2(50);
+	//task_3();
+	//task_4();
+	//task_5();
+	//task_6();
+	//task_7();
+	task_8();
+	system("pause");
+}
