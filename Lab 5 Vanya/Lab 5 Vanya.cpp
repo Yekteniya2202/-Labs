344de07dcdda51adafa2c58e6ad8addc0a8df485
Yekteniya2202 <yellowblue924@yandex.ru>
2021-10-17 23:56:06 +0300

    Lab 5 tmp work

---
+++ Lab 5 Vanya/Lab 5 Vanya.cpp
@@ -0,0 +1,177 @@
+﻿Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+
+@Yekteniya2202
+Vaschilo
+/
+APXUTEKTYPA
+Public
+1
+10
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+APXUTEKTYPA / 5.ompSections / 5.ompSections / main.cpp
+@Vaschilo
+Vaschilo 5opka
+Latest commit 847fb97 11 days ago
+History
+1 contributor
+147 lines(129 sloc)  3.08 KB
+
+#include <iostream>
+using namespace std;
+#include <omp.h>
+#include <vector>
+#include <string>
+#include <functional>
+
+void vectorSum();
+void startSort(int* arr, int n, string sort_name, function<void(int*, int)> func);
+
+void Merge(int* arr, int from, int to) {
+	int middle, start, final, j;
+	int* helperArr = new int[to + 1];
+	middle = (from + to) / 2;
+	start = from;
+	final = middle + 1;
+	for (j = from; j <= to; j++) {
+		if ((start <= middle) && ((final > to) || (arr[start] < arr[final]))) {
+			helperArr[j] = arr[start]; start++;
+		}
+		else {
+			helperArr[j] = arr[final];
+			final++;
+		}
+	}
+	for (j = from; j <= to; j++)
+		arr[j] = helperArr[j];
+	delete[] helperArr;
+};
+void SimpleMergeSort(int* arr, int from, int to) {
+	{
+		if (from < to) {
+			SimpleMergeSort(arr, from, (from + to) / 2);
+			SimpleMergeSort(arr, (from + to) / 2 + 1, to);
+			Merge(arr, from, to);
+		}
+	}
+};
+void MergeSortTrue(int* arr, int from, int to) {
+	if (from >= to) return;
+#pragma omp parallel num_threads(10)
+	{
+#pragma omp sections
+		{
+#pragma omp section
+			{
+				MergeSortTrue(arr, from, (from + to) / 2);
+			}
+#pragma omp section
+			{
+				MergeSortTrue(arr, (from + to) / 2 + 1, to);
+			}
+		}
+		Merge(arr, from, to);
+	}
+}
+
+
+void sorts() {
+	int n = 0;
+	srand(time(NULL));
+	while (n <= 0)
+	{
+		cout << "Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ â ìàññèâå: ";
+		cin >> n;
+		cout << endl;
+	}
+
+	int* myArray = new int(n);
+
+	for (int i = 0; i < n; i++) myArray[i] = rand() % 2001 - 1000; // Îò -1000 äî 1000
+	for (int i = 0; i < n; i++) cout << myArray[i] << "\t";
+
+	startSort(myArray, n, "Îáû÷íàÿ ñîðòèðîâêà ñëèÿíèåì(Ïîñëåäîâàòåëüíûé àëãîðèòì)",
+		[](int* arr, int size) {SimpleMergeSort(arr, 0, size - 1); });
+	startSort(myArray, n, "Ñîðòèðîâêà ñëèÿíèåì ñî âëîæåííûì ïàðàëëåëèçìîì",
+		[](int* arr, int size) {omp_set_nested(1); MergeSortTrue(arr, 0, size - 1); });
+
+	delete[] myArray;
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	auto f = []() {cout << "\n\n\n"; system("pause"); system("cls"); };
+
+	//vectorSum(); f();
+	sorts(); f();
+
+	return 0;
+}
+
+void vectorSum() {
+	int n = 0;
+	while (n <= 0)
+	{
+		cout << "Ââåäèòå äëèíó âåêòîðà: ";
+		cin >> n;
+		cout << endl;
+	}
+	vector<int> a; a.resize(n);
+	vector<int> b; b.resize(n);
+	vector<int> c; c.resize(n);
+	auto f = [n](vector<int>& a, char name) {
+		for (int i = 0; i < n; i++)
+		{
+			cout << name << "[" << i << "] = ";
+			cin >> a[i];
+		}
+	};
+	f(a, 'a');
+	f(b, 'b');
+#pragma omp parallel num_threads(3)
+	{
+#pragma omp sections
+		{
+#pragma omp section
+			{
+				for (int i = 0; i < n / 2; i++) c[i] = a[i] + b[i];
+			}
+#pragma omp section
+			{
+				for (int i = n / 2; i < n; i++) c[i] = a[i] + b[i];
+			}
+		}
+	}
+	cout << "Result: \n";
+	for (int i = 0; i < n; i++)
+		cout << "c[" << i << "] = " << c[i] << endl;
+}
+
+void startSort(int* arr, int n, string sort_name, function<void(int*, int)> func)
+{
+	int* copy = new int(n);
+	for (int i = 0; i < n; i++) copy[i] = arr[i];
+
+	cout << "\n\n" << sort_name << endl;
+	double from = omp_get_wtime();
+
+	func(copy, n);
+
+	double to = omp_get_wtime();
+	cout << "Âðåìÿ âûïîëíåíèÿ - " << to - from << endl << "Ðåçóëüòàò:\n";
+	for (int i = 0; i < n; i++) cout << copy[i] << "\t";
+
+	delete[] copy;
+}
---
+++ Lab 5/Lab 5.cpp
@@ -0,0 +1,255 @@
+﻿// Lab 5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <omp.h>
+#include <stack>
+#include <functional>
+using namespace std;
+
+vector<double> vector_sum_multi(const vector<int>& a,
+	const vector<int>& b,
+	bool multithreading) {
+
+	if (a.size() != b.size()) {
+		throw exception("Размеры массива не совпадают");
+	}
+
+	const int len = a.size();
+	vector<double> c(len);
+	if (multithreading) {
+		#pragma omp parallel num_threads(2)
+		{
+			#pragma omp sections
+			{
+				#pragma omp section
+				{
+					for (int i = 0; i < len / 2; i++) {
+						c[i] = a[i] + b[i];
+					}
+				}
+				#pragma omp section
+				{
+					for (int i = len / 2; i < len; i++) {
+						c[i] = a[i] + b[i];
+					}
+				}
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < len; i++) {
+			c[i] = a[i] + b[i];
+		}
+	}
+
+	return c;
+}
+
+
+void merge_sort_nonmulti(vector<int>& a, int start, int end) {
+	if (end - start < 2)
+		return;
+	if (end - start == 2) {
+		if (a[start] > a[start + 1])
+			swap(a[start], a[start + 1]);
+		return;
+	}
+	merge_sort_nonmulti(a, start, start + (end - start) / 2);
+	merge_sort_nonmulti(a, start + start + (end - start) / 2, end);
+	vector<int> b;
+	//начало
+	int b1 = start;
+	//середина
+	int e1 = start + (end - start) / 2;
+	int b2 = e1;
+	while (b.size() < end - start)
+	{
+		if (b1 >= e1 || (b2 < end && a[b2] <= a[b1])) {
+			b.push_back(a[b2]);
+			b2++;
+		}
+		else {
+			b.push_back(a[b1]);
+			b1++;
+		}
+	}
+	for (int i = start; i < end; i++) {
+		a[i] = b[i - start];
+	}
+
+}
+
+void merge_sort_multi(vector<int>& a, int start, int end)
+{
+	if (end - start < 2)
+		return;
+	if (end - start == 2) {
+		if (a[start] > a[start + 1])
+			swap(a[start], a[start + 1]);
+		return;
+	}
+	#pragma omp parallel 
+	{
+		#pragma omp sections
+		{
+			#pragma omp section
+			{
+				merge_sort_multi(a, start, start + (end - start) / 2);
+			}
+			#pragma omp section
+			{
+				merge_sort_multi(a, start + start + (end - start) / 2, end);
+			}
+		}
+
+
+		#pragma omp sections
+		{
+			#pragma omp section
+			{
+				vector<int> b;
+				//начало
+				int b1 = start;
+				//середина
+				int e1 = start + (end - start) / 2;
+				int b2 = e1;
+				while (b.size() < end - start)
+				{
+					if (b1 >= e1 || (b2 < end && a[b2] <= a[b1])) {
+						b.push_back(a[b2]);
+						b2++;
+					}
+					else {
+						b.push_back(a[b1]);
+						b1++;
+					}
+				}
+				for (int i = start; i < end; i++) {
+					a[i] = b[i - start];
+				}
+			}
+		}
+	}
+	
+} 
+
+int partition(vector<int>& a, int start, int end)
+{
+	int pivot = a[end];    // pivot 
+	int i = (start - 1);
+
+	for (int j = start; j <= end - 1; j++)
+	{
+		//if current element is smaller than pivot, increment the low element
+		//swap elements at i and j
+		if (a[j] <= pivot)
+		{
+			i++;    // increment index of smaller element 
+			swap(a[i], a[j]);
+		}
+	}
+	swap(a[i + 1], a[end]);
+	return (i + 1);
+}
+
+
+//quicksort algorithm
+void quickSort_nonmulti(vector<int>& a, int start, int end)
+{
+	if (start < end)
+	{
+		//partition the array 
+		int pivot = partition(a, start, end);
+
+		//sort the sub arrays independently 
+		quickSort_nonmulti(a, start, pivot - 1);
+		quickSort_nonmulti(a, pivot + 1, end);
+	}
+}
+
+//quicksort algorithm
+void quickSort_multi(vector<int>& a, int start, int end)
+{
+	if (start < end)
+	{
+		int pivot = partition(a, start, end);
+		#pragma omp parallel num_threads(2)
+		{
+			#pragma omp sections
+			{
+				#pragma omp section
+				{
+					//sort the sub arrays independently 
+					quickSort_nonmulti(a, start, pivot - 1);
+				}
+				#pragma omp section
+				{
+					quickSort_nonmulti(a, pivot + 1, end);
+				}
+			}
+		}
+	}
+}
+
+void shuffle(vector<int>& a) {
+	for (auto& elem : a) {
+		elem = rand() % 1000;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	omp_set_dynamic(0);
+	omp_set_num_threads(8);
+	vector<int> a(1000000);
+	vector<int> b;
+	vector<int> c;
+	double start, end;
+	shuffle(a);
+	b = a;
+	c = a;
+
+	start = omp_get_wtime();
+	vector_sum_multi(a, b, false);
+	end = omp_get_wtime();
+	cout << "Сумма векторов в последовательном режиме -> " << end - start << endl;
+
+	start = omp_get_wtime();
+	vector_sum_multi(a, b, true);
+	end = omp_get_wtime();
+	cout << "Сумма векторов в параллельном режиме -> " << end - start << endl;
+
+
+	start = omp_get_wtime();
+	merge_sort_nonmulti(a, 0, a.size() - 1);
+	end = omp_get_wtime();
+	cout << "Cортировка слиянием в последовательном режиме -> " << end - start << endl;
+
+	omp_set_nested(false);
+	start = omp_get_wtime();
+	merge_sort_multi(b, 0, b.size() - 1);
+	end = omp_get_wtime();
+	cout << "Cортировка слиянием в параллельном режиме без вложенности пар-зма -> " << end - start << endl;
+
+	omp_set_nested(true);
+	start = omp_get_wtime();
+	merge_sort_multi(c, 0, c.size() - 1);
+	end = omp_get_wtime();
+	cout << "Cортировка слиянием в параллельном режиме с вложенным пар-змом-> " << end - start << endl;
+	
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
